34a77a6564efbf5c2d0cf9d5b4d592b7fbf0a1d5
brunocoronado49 <francorangelcoronado@gmail.com>
2026-06-06 22:42:41 -0600

    ✨ Instal class validator, create crud of pokemons

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: `${__dirname}/../public`,
+    }),
+    MongooseModule.forRoot('mongodb://brucedev:123456@localhost:27017/'),
+    PokemonModule,
+  ],
   controllers: [],
   providers: [],
+  exports: [],
 })
 export class AppModule {}
