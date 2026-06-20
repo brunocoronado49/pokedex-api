870ceaf6e34c9ffd86018153b2f119b01e9d735f
brunocoronado49 <francorangelcoronado@gmail.com>
2026-06-19 20:36:40 -0600

    ✨ Add pagination changes in get all pokemons

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -9,6 +9,7 @@ import { Model, isValidObjectId } from 'mongoose';
 import { CreatePokemonDto } from './dtos/create-pokemon.dto';
 import { UpdatePokemonDto } from './dtos/update-pokemon.dto';
 import { Pokemon } from './entities/pokemon.entity';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
@@ -28,8 +29,14 @@ export class PokemonService {
     }
   }
 
-  async findAll() {
-    const pokemons = await this.pokemonModel.find();
+  async findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
+    const pokemons = await this.pokemonModel
+      .find()
+      .limit(limit)
+      .skip(offset)
+      .sort({ no: 1 })
+      .select('-__v');
 
     return pokemons;
   }
